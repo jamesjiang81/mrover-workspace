c7efa25feb346523370b5e929ad2720d2e1bc7cc
rzf16 <ryan.feng16@gmail.com>
2019-09-29 15:37:02 -0400

    [Localization] Created program to process, present, and publish GPS and IMU data to the LCM.

--- onboard/filter/src/logger.py
+++ onboard/filter/src/logger.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import os
+from os import getenv
 import time
 
 from rover_common import aiolcm
@@ -63,6 +64,13 @@ class Logger:
         # Temp mov_avg filter
         self.mov_avg_millis = time.time() * 1000
 
+        # Initialize sensor timestamps
+        self.gps_millis = time.time() * 1000
+        self.phone_millis = time.time() * 1000
+        self.imu_millis = time.time() * 1000
+        self.nav_status_millis = time.time() * 1000
+        self.odom_millis = time.time() * 1000
+
     def write(self, contents, type):
         # Writes contents to the log specified by type
         # with open(self.file_path + type + 'Log.csv', 'w') as log:
@@ -73,11 +81,67 @@ class Logger:
 
     def gps_callback(self, channel, msg):
         gps = GPS.decode(msg)
+<<<<<<< HEAD
         if (time.time()*1000 - self.gps_millis) > \
                 self.logConfig['rate_millis']['gps']:
             self.write([gps.latitude_deg, gps.latitude_min, gps.longitude_deg,
                         gps.longitude_min, gps.bearing_deg, gps.speed], 'gps')
             self.gps_millis = time.time()*1000
+
+    def phone_callback(self, channel, msg):
+        phone = SensorPackage.decode(msg)
+        if (time.time()*1000 - self.phone_millis) > \
+                self.logConfig['rate_millis']['phone']:
+            self.write([phone.latitude_deg, phone.latitude_min,
+                        phone.longitude_deg, phone.longitude_min,
+                        phone.bearing, phone.speed], 'phone')
+            self.phone_millis = time.time()*1000
+
+    def imu_callback(self, channel, msg):
+        imu = IMU.decode(msg)
+        if (time.time()*1000 - self.imu_millis) > \
+                self.logConfig['rate_millis']['imu']:
+            self.write([imu.accel_x, imu.accel_y, imu.accel_z, imu.gyro_x,
+                        imu.gyro_y, imu.gyro_z, imu.mag_x, imu.mag_y,
+                        imu.mag_z, imu.bearing], 'imu')
+            self.imu_millis = time.time()*1000
+
+    def nav_status_callback(self, channel, msg):
+        nav_status = NavStatus.decode(msg)
+        if (time.time()*1000 - self.nav_status_millis) > \
+                self.logConfig['rate_millis']['nav_status']:
+            self.write([nav_status.nav_state, nav_status.nav_state_name,
+                        nav_status.completed_wps, nav_status.missed_wps,
+                        nav_status.total_wps, nav_status.found_tbs,
+                        nav_status.total_tbs], 'navStatus')
+            self.nav_status_millis = time.time()*1000
+
+    def odom_callback(self, channel, msg):
+<<<<<<< HEAD
+        odom = Odometry.decode(msg)
+        if (time.time()*1000 - self.odom_millis) > \
+                self.logConfig['rate_millis']['odom']:
+            self.write([odom.latitude_deg, odom.latitude_min,
+                        odom.longitude_deg, odom.longitude_min,
+                        odom.bearing_deg, odom.speed], 'odom')
+            self.odom_millis = time.time()*1000
+
+    def mov_avg_callback(self, channel, msg):
+        mov_avg = Odometry.decode(msg)
+        if (time.time()*1000 - self.mov_avg_millis) > \
+                self.logConfig['rate_millis']['odom']:
+            self.write([mov_avg.latitude_deg, mov_avg.latitude_min,
+                        mov_avg.longitude_deg, mov_avg.longitude_min,
+                        mov_avg.bearing_deg, mov_avg.speed], 'movAvg')
+            self.mov_avg_millis = time.time()*1000
+
+
+if __name__ == "__main__":
+    logger = Logger()
+            self.write([gps.latitude_deg, gps.latitude_min, gps.longitude_deg,
+                        gps.longitude_min, gps.bearing_deg, gps.speed], 'gps')
+            self.gps_millis = time.time()*1000
+>>>>>>> Add random data generator for plotter testing. Minor bug fixes in logger. Python linter will hate me. It's ok though, the hatred is mutual.
 
     def phone_callback(self, channel, msg):
         phone = SensorPackage.decode(msg)
@@ -116,15 +180,7 @@ class Logger:
                         odom.bearing_deg, odom.speed], 'odom')
             self.odom_millis = time.time()*1000
 
-    def mov_avg_callback(self, channel, msg):
-        mov_avg = Odometry.decode(msg)
-        if (time.time()*1000 - self.mov_avg_millis) > \
-                self.logConfig['rate_millis']['odom']:
-            self.write([mov_avg.latitude_deg, mov_avg.latitude_min,
-                        mov_avg.longitude_deg, mov_avg.longitude_min,
-                        mov_avg.bearing_deg, mov_avg.speed], 'movAvg')
-            self.mov_avg_millis = time.time()*1000
-
 
 if __name__ == "__main__":
     logger = Logger()
+>>>>>>> Working logger w/ toods
