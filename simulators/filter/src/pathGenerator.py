e512bf026f2b50c626fcb88f927b8551deeecb76
david <dleroys@umich.edu>
2020-01-16 23:40:35 -0500

    found a good resource

--- simulators/filter/src/pathGenerator.py
+++ simulators/filter/src/pathGenerator.py
@@ -55,8 +55,8 @@ class PathGenerator:
         y = np.array([83.737,83.739,83.739,83.740,83.738,83.736,83.736,83.735,83.735,83.732,83.733,83.731,83.730,83.725,83.724,83.723,83.724,83.721,83.723,83.727,83.728,83.733,83.732,83.733,
                       83.735, 83.737,83.740, 83.742, 83.741, 83.744, 83.745, 83.747, 83.750, 83.753, 83.751, 83.749, 83.748, 83.747,83.748,83.748])
         tck = interpolate.splrep(x, y, s=100)
-        smooth_y = np.linspace(42.277,42.292, num=100)
-        smooth_x = interpolate.splev(xnew, tck, der=0)
+        smooth_x = np.linspace(42.277,42.292, num=100)
+        smooth_y = interpolate.splev(smooth_x, tck, der=0)
 
     # generates accelerations in m/s2
     def point_gen(self):
